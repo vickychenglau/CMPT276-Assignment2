5f785729d8683531b54889642ad336039c7bf740
vcl2 <vcl2@sfu.ca>
2017-09-29 19:26:58 -0700

    static pages up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
